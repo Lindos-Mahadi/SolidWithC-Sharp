4e45e71250e3ce32b80fe2a2b5525ef839f8a7f9
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-10-29 13:24:50 +0600

    Single Responsibility Principle

---
+++ SolidWithC-Sharp/IUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidWithC_Sharp
+{
+    // Note: Do not declare all method in a single interface.
+    interface IUser
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password, string email);
+    }
+
+    interface ILoger
+    {
+        void LogError(string error);
+        bool Register(string username, string password, string email);
+    }
+
+    interface IEmail
+    {
+        bool SendEmail(string emailContent);
+    }
+}
---
+++ SolidWithC-Sharp/SOLID-Advantages.cs
@@ -0,0 +1,11 @@
+﻿Defination : Single Responsibility Priciple
+Robert C. Martin describes it as:
+A class should have one, and only one, reason to change.
+
+Advantages of SOLID
+1)Maintainability
+2)Testability
+3)Flexibility and Extensibility
+4)Parallel Development
+5)Loose Coupling
+6)SOLID Principles and Desing Patterns Plays key role in the above motivations
